ed293f0ed13d32ddb436ed3e59abaeba2bc3d21e
Olek Janiszewski <olek.janiszewski@gmail.com>
2016-10-10 18:14:08 +0200

    Remove puts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'shameless'
 
-Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each {|file| puts file; require file }
+Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each {|file| require file }
